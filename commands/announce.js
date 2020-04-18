8e7f553200a829233ce3a999a338db93271e00f3
Alexandre <43376983+AlexBrunGiglio@users.noreply.github.com>
2020-04-19 00:32:38 +0200

    Annonce 1.0.2

--- commands/announce.js
+++ commands/announce.js
@@ -16,12 +16,13 @@ module.exports = class Announce extends Command {
     switch(args[0]){
       case 'an' :
         const embed = new Discord.MessageEmbed()
-        .setTitle('Hello le WAG !')
-        .setColor(0x2EC5E7)
-        .setDescription("Après des mois d'absence sur le serveur je suis de retour avec de toutes nouvelles fonctionnalités ! \n En effet je serai en ligne sur le serveur pour vous voir vous amuser et aussi vous aider en cas de besoin. \n  Sachez que je serai en constante évolution sur le serveur et que je risque parfois de m'absenter pour ajouter mes nouvelles fonctionnalités. \n \n Dorénavant tout les nouveaux membres du serveur recevront ce message à leur arrivée !")
+        .setTitle('Bonjour à tous !')
+        .setColor(0x72c8e2)
+        .setDescription("Je viens vous apporter une superbe nouvelle ! \nComme vous le savez peut-être il y a sur le serveur un bot qui se nomme <@302050872383242240>. \n \nLe principe est de mettre tout les serveur le possédant sur un site internet. Cependant les serveurs qui utilise la commande '!d bump' sont mit en avant pour une durée de 2h.\n \nC'est pourquoi pour vous encourager à bumper nous avons créé un système de trophées ! \nTout les mois nous comptabiliserons vos bumps, ceux qui arriveront à bumper au moins 50 fois décrocheront le rôle <@&701195227393949747>  et devront remettre leurs titres en jeu le mois d'après !\n\nBon courage à tout le monde ! \n")
         // .addField('Serveur', message.guild.name)
-        .setThumbnail('https://images-ext-1.discordapp.net/external/8ejf4mlVIWzN2qrbiSPnHEMrQ_NvG72nes1Kn6VGTMA/https/media.discordapp.net/attachments/416477999698018314/689834041326698627/t.gif')
+        // .setThumbnail('https://images-ext-2.discordapp.net/external/kZlLHMBW7JS-FGa7mMNKQIGCyRi2-oHGVy35YguGRPQ/%3Fsize%3D256/https/cdn.discordapp.com/avatars/302050872383242240/67342a774a9f2d20d62bfc8553bb98e0.png')
         // .addField('Player Name', message.author.username);
+        .setImage('https://images-ext-1.discordapp.net/external/tAuRcs-FCy2M8OaTS9Ims62J1vrFiviahjBDtpZrrBs/https/disboard.org/images/bot-command-image-bump.png')
         message.channel.send(embed);
       break;
     }
